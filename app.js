746095b55550b77f5ae9c1e74b36b873a1603aa4
Primemerlinian <mwatson0518@gmail.com>
2022-12-19 14:30:32 -0500

    add event listeners

--- app.js
+++ app.js
@@ -16,6 +16,12 @@
 
 /*----------------------------- Event Listeners -----------------------------*/
 
+begin.addEventListener('click', beginGame)
+
+attention.addEventListener('click', handleClick)
+food.addEventListener('click', handleClick)
+care.addEventListener('click', handleClick)
+play.addEventListener('click', handleClick)
 
 
 
