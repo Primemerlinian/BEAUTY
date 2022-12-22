0bb3428c16261ecbcc15c6a72fc9c727f28b6b0f
Primemerlinian <mwatson0518@gmail.com>
2022-12-22 16:05:51 -0500

    reset button functional

--- app.js
+++ app.js
@@ -13,11 +13,10 @@ const demands = [
 
 /*-------------------------------- Variables --------------------------------*/
 
-let timer
 let timeLeft= 60;
 let progress= 0
-let demandLoser = false
-let demandTime = 60
+let time
+
 
 
 
@@ -32,11 +31,10 @@ const attention = document.getElementById('attention')
 const food = document.querySelector('#food')
 const care = document.querySelector('#care')
 const ball = document.querySelector('#ball')
-// const resetButton = document.querySelectorAll('#reset')
+const resetButton = document.getElementById('reset-button')
 const countdownEl = document.getElementById('countdown')
-const progBar = document.querySelectorAll('progressBar')
-let demandNum = Math.floor(Math.random()* demands.length)
-// console.log(demands[demandNum])
+const prog = document.querySelectorAll('progress')
+let demandNum = Math.floor(Math.random() * demands.length)
 const calmSound = new Audio ("../assets/audio/calmgameplay.wav")
 let winner = document.querySelector('#winner')
 let loser = document.querySelector('#loser')
@@ -50,7 +48,7 @@ let loser = document.querySelector('#loser')
 
 beginButton.addEventListener('click', begin)
 
-// resetButton.addEventListener('click', init)
+resetButton.addEventListener('click', init)
 
 attention.addEventListener('click', demandAttention)
 
@@ -68,9 +66,17 @@ init ()
 
 function init() {
   stopPlaying = false
-  timeLeft = demandTime
-  resetButton.setAttribute('shown',true)
+  resetButton.style.visibility = 'visible';
+  console.log('resetclick');
+  timeLeft= 60
+  progress= 0
+  messageEl.innerText = ''
+  countdownEl.innerText = ''
+  beginButton.style.visibility = 'visible';
+  winner.innerText = '';
+  loser.innerText = '';
   calmSound.pause()
+  clearInterval(time)
 }
 
 function randomDemands () {
@@ -80,10 +86,11 @@ function randomDemands () {
 function begin () {
   let text = randomDemands ()
   messageEl.innerHTML = text
-  beginButton.setAttribute('hidden', true)
-
+  // beginButton.setAttribute('hidden', true)
+  beginButton.style.visibility = 'hidden';
   calmSound.volume = 0.20
   calmSound.play()
+  startTimer()
   
 }
 
@@ -122,6 +129,7 @@ function demandBall () {
 
 
 function winnerCheck() {
+  console.log(progress);
   if (progress >= 90) {
     winner.removeAttribute('hidden')
     messageEl.setAttribute('hidden', true)
@@ -131,18 +139,18 @@ function winnerCheck() {
     loser.removeAttribute('hidden')
     stopPlaying = true
     calmSound.pause()
-
+    
   }
+
 }
-
-
-let time = setInterval(function() {
-    countdownEl.textContent = timeLeft + ' seconds remaining.';
-    timeLeft -= 1;
-    if (timeLeft < 0) {
-          countdownEl.textContent = 'Demand time complete'
-      }
-    }, 1000)
-
+  function startTimer(){
+    time = setInterval(function() {
+        countdownEl.textContent = timeLeft + ' seconds remaining.';
+        timeLeft -= 1;
+        if (timeLeft < 0) {
+              countdownEl.textContent = 'Demand time complete'
+          }
+        }, 1000)
+  }
 
 
