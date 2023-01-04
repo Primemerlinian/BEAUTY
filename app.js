711c0ffc3c653323e61e6f2c2361f3f64b5f416c
Primemerlinian <mwatson0518@gmail.com>
2023-01-03 20:15:33 -0500

    correct indentation and add semicolons

--- app.js
+++ app.js
@@ -1,132 +1,119 @@
-
 const demands = [
-  'I need attention 🥺🐾',
-  'I am hungry 😡',
-  'Clean my teeth 🦷',
-  'Play ball with me 🎾'
-]
+  "I need attention 🥺🐾",
+  "I am hungry 😡",
+  "Clean my teeth 🦷",
+  "Play ball with me 🎾",
+];
 
-
-let timeLeft= 40;
-let progress= 0
-let time
+let timeLeft = 40;
+let progress = 0;
+let time;
 let scoreText;
 
-
-const beginButton = document.getElementById('begin-button')
-const messageEl = document.getElementById('message')
-const attention = document.getElementById('attention')
-const food = document.querySelector('#food')
-const care = document.querySelector('#care')
-const ball = document.querySelector('#ball')
-const resetButton = document.getElementById('reset-button')
-const countdownEl = document.getElementById('countdown')
-const prog = document.querySelectorAll('progress')
-let demandNum = Math.floor(Math.random() * demands.length)
-const calmSound = new Audio ("../assets/audio/calmgameplay.wav")
-let winner = document.querySelector('#winner')
-let loser = document.querySelector('#loser')
+const beginButton = document.getElementById("begin-button");
+const messageEl = document.getElementById("message");
+const attention = document.getElementById("attention");
+const food = document.querySelector("#food");
+const care = document.querySelector("#care");
+const ball = document.querySelector("#ball");
+const resetButton = document.getElementById("reset-button");
+const countdownEl = document.getElementById("countdown");
+const prog = document.querySelectorAll("progress");
+let demandNum = Math.floor(Math.random() * demands.length);
+const calmSound = new Audio("../assets/audio/calmgameplay.wav");
+let winner = document.querySelector("#winner");
+let loser = document.querySelector("#loser");
 const score = document.querySelector("#score");
 
+beginButton.addEventListener("click", begin);
 
+resetButton.addEventListener("click", init);
 
-beginButton.addEventListener('click', begin)
+attention.addEventListener("click", demandAttention);
 
-resetButton.addEventListener('click', init)
+food.addEventListener("click", demandFood);
 
-attention.addEventListener('click', demandAttention)
+care.addEventListener("click", demandCare);
 
-food.addEventListener('click', demandFood)
+ball.addEventListener("click", demandBall);
 
-care.addEventListener('click', demandCare)
-
-ball.addEventListener('click', demandBall)
-
-
-
-
-init ()
+init();
 
 function init() {
-  resetButton.style.visibility = 'visible';
-  timeLeft= 60
-  progress= 0
-  messageEl.innerText = ''
-  countdownEl.innerText = ''
-  beginButton.style.visibility = 'visible';
-  winner.setAttribute('hidden', true)
-  loser.setAttribute('hidden', true)
+  resetButton.style.visibility = "visible";
+  timeLeft = 60;
+  progress = 0;
+  messageEl.innerText = "";
+  countdownEl.innerText = "";
+  beginButton.style.visibility = "visible";
+  winner.setAttribute("hidden", true);
+  loser.setAttribute("hidden", true);
   score.innerHTML = `Score:  0`;
-  calmSound.pause()
-  clearInterval(time)
+  calmSound.pause();
+  clearInterval(time);
 }
 
-function randomDemands () {
-  let randomNum =  [Math.floor(Math.random() * demands.length)]
-  return messageEl.innerHTML = demands[randomNum]
+function randomDemands() {
+  let randomNum = [Math.floor(Math.random() * demands.length)];
+  return (messageEl.innerHTML = demands[randomNum]);
 }
-function begin () {
-  let text = randomDemands ()
-  messageEl.innerHTML = text
-  beginButton.style.visibility = 'hidden';
-  calmSound.volume = 0.20
-  calmSound.play()
-  startTimer()
-  
+function begin() {
+  let text = randomDemands();
+  messageEl.innerHTML = text;
+  beginButton.style.visibility = "hidden";
+  calmSound.volume = 0.2;
+  calmSound.play();
+  startTimer();
 }
 
 function demandAttention() {
-  if(messageEl.innerHTML === demands[0]) {
-    progress += 10
-    randomDemands()
+  if (messageEl.innerHTML === demands[0]) {
+    progress += 10;
+    randomDemands();
   }
-  winnerCheck()
+  winnerCheck();
 }
 function demandFood() {
   if (messageEl.innerHTML === demands[1]) {
-    progress +=10
-    randomDemands()
+    progress += 10;
+    randomDemands();
   }
-  winnerCheck()
+  winnerCheck();
 }
-function demandCare () {
+function demandCare() {
   if (messageEl.innerHTML === demands[2]) {
-    progress += 10
-    randomDemands ()
+    progress += 10;
+    randomDemands();
   }
-  winnerCheck()
+  winnerCheck();
 }
-function demandBall () {
-    if (messageEl.innerHTML === demands[3]) {
-      progress += 10
-      randomDemands ()
-    }
-    winnerCheck()
+function demandBall() {
+  if (messageEl.innerHTML === demands[3]) {
+    progress += 10;
+    randomDemands();
+  }
+  winnerCheck();
 }
-
 
 function winnerCheck() {
   if (progress >= 90) {
-    winner.removeAttribute("hidden")
-    messageEl.setAttribute('hidden', true)
-    clearInterval(time)
-    calmSound.pause()
-  } else if ((progress <= 0) || timeLeft === 0) {
-    loser.removeAttribute('hidden')
-    clearInterval(time)
-    calmSound.pause()
+    winner.removeAttribute("hidden");
+    messageEl.setAttribute("hidden", true);
+    clearInterval(time);
+    calmSound.pause();
+  } else if (progress <= 0 || timeLeft === 0) {
+    loser.removeAttribute("hidden");
+    clearInterval(time);
+    calmSound.pause();
   }
 }
 
-
-  function startTimer(){
-    time = setInterval(function() {
-        countdownEl.textContent = timeLeft + ' seconds remaining.';
-        timeLeft -= 1;
-        if (timeLeft < 0) {
-              countdownEl.textContent = 'Demand time complete'
-          }
-        }, 1000)
-  }
-
-
+function startTimer() {
+  time = setInterval(function () {
+    countdownEl.textContent = timeLeft + " seconds remaining.";
+    timeLeft -= 1;
+    if (timeLeft < 0) {
+      countdownEl.textContent = "Demand time complete";
+    }
+  }, 1000);
+}
